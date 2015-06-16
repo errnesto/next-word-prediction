bc97706ae8e6c3d946f9aa3c22a69e55340b215f
Manuel Reich <errnesto@me.com>
2015-06-16 09:38:58 +0200

    error in self transition unigrams

--- models/Learner.py
+++ models/Learner.py
@@ -26,8 +26,8 @@ class Learner():
     predecessor_groups_of = defaultdict(set) # word : {pre_group}
     succsessor_groups_of  = defaultdict(set) # word : {succ_group}
 
-    word_predecessing_group_count = defaultdict(int) # group : word_count
-    word_succsessing_group_count  = defaultdict(int) # group : word_count
+    predecessor_word_count_of_group = defaultdict(int) # group : word_count
+    succsessor_word_count_of_group  = defaultdict(int) # group : word_count
 
     wordEntropy = 0
 
@@ -109,7 +109,7 @@ class Learner():
 
         for word, wordData in self.vocabulary.iteritems():
             #count groups
-            self.group_counts[wordData.group] += 1
+            self.group_counts[wordData.group] += wordData.count
 
             predecessors = self.predecessors_of[word]
             for pre_word in predecessors:
@@ -134,10 +134,10 @@ class Learner():
             self.wordEntropy += wordData.count * math.log(wordData.count)
 
     def update_help_counts(self, word):
-        self.word_predecessing_group_count = defaultdict(int)
-        self.word_succsessing_group_count  = defaultdict(int)
-        self.predecessor_groups_of[word]   = set()
-        self.succsessor_groups_of[word]    = set()
+        self.predecessor_word_count_of_group = defaultdict(int)
+        self.succsessor_word_count_of_group  = defaultdict(int)
+        self.predecessor_groups_of[word]     = set()
+        self.succsessor_groups_of[word]      = set()
 
         predecessors = self.predecessors_of[word]
         successors   = self.successors_of[word]
@@ -147,59 +147,57 @@ class Learner():
             bigram_count   = self.successors_of[pre_word][word]
             pre_word_group = self.vocabulary[pre_word].group
 
-            self.word_predecessing_group_count[group] += bigram_count
+            self.succsessor_word_count_of_group[pre_word_group] += bigram_count
             self.predecessor_groups_of[word].add(pre_word_group)
 
         for succ_word, count in successors.iteritems():
-            succ_word_group = self.vocabulary[word].group
+            succ_word_group = self.vocabulary[succ_word].group
 
-            self.word_succsessing_group_count[group] += count
+            self.predecessor_word_count_of_group[succ_word_group] += count
             self.succsessor_groups_of[word].add(succ_word_group)
+        
+    def move_word(self, word, new_group):
+        old_group = self.vocabulary[word].group
+        self.vocabulary[word].group = new_group
 
-    def remove_word_from_group(self, word, old_group):
         for group in self.predecessor_groups_of[word]:
             if group != old_group:
                 group_bigramm_name = str(group) + "_" + str(old_group)
-                self.group_bigramm_counts[group_bigramm_name] -= self.word_succsessing_group_count[group]
+                self.group_bigramm_counts[group_bigramm_name] -= self.succsessor_word_count_of_group[group]
+
+            if group != new_group:
+                group_bigramm_name = str(group) + "_" + str(new_group)
+                self.group_bigramm_counts[group_bigramm_name] += self.succsessor_word_count_of_group[group]
 
         for group in self.succsessor_groups_of[word]:
             if group != old_group: 
                 group_bigramm_name = str(old_group) + "_" + str(group)
-                self.group_bigramm_counts[group_bigramm_name] -= self.word_predecessing_group_count[group]
+                self.group_bigramm_counts[group_bigramm_name] -= self.predecessor_word_count_of_group[group]
+
+            if group != new_group:
+                group_bigramm_name = str(new_group) + "_" + str(group)
+                self.group_bigramm_counts[group_bigramm_name] += self.predecessor_word_count_of_group[group]
 
         self.group_counts[old_group] -= self.vocabulary[word].count
+        self.group_counts[new_group] += self.vocabulary[word].count
 
         self_bigramm_name = str(old_group) + "_" + str(old_group)
         self.group_bigramm_counts[self_bigramm_name] = (self.group_bigramm_counts[self_bigramm_name] 
-                                                        - self.word_succsessing_group_count[old_group]
-                                                        - self.word_predecessing_group_count[old_group]
+                                                        - self.succsessor_word_count_of_group[old_group]
+                                                        - self.predecessor_word_count_of_group[old_group]
                                                         + self.successors_of[word][word])
-
-        # self.word_predecessing_group_count[old_group] -= self.successors_of[word][word]
-        # self.word_succsessing_group_count[old_group]  -= self.successors_of[word][word]
-
-    def add_word_to_group(self, word, new_group):
-        for group in self.predecessor_groups_of[word]:
-            if group != new_group:
-                group_bigramm_name = str(group) + "_" + str(new_group)
-                self.group_bigramm_counts[group_bigramm_name] += self.word_succsessing_group_count[group]
-
-        for group in self.succsessor_groups_of[word]:
-            if group != new_group: 
-                group_bigramm_name = str(new_group) + "_" + str(group)
-                self.group_bigramm_counts[group_bigramm_name] += self.word_predecessing_group_count[group]
-
-        self.group_counts[new_group] += self.vocabulary[word].count
 
         self_bigramm_name = str(new_group) + "_" + str(new_group)
         self.group_bigramm_counts[self_bigramm_name] = (self.group_bigramm_counts[self_bigramm_name] 
-                                                        + self.word_succsessing_group_count[new_group]
-                                                        + self.word_predecessing_group_count[new_group]
+                                                        + self.succsessor_word_count_of_group[new_group]
+                                                        + self.predecessor_word_count_of_group[new_group]
                                                         - self.successors_of[word][word])
 
-        # self.word_predecessing_group_count[new_group] += self.successors_of[word][word]
-        # self.word_succsessing_group_count[new_group]  += self.successors_of[word][word]
-        
+        self.predecessor_word_count_of_group[old_group] -= self.successors_of[word][word]
+        self.succsessor_word_count_of_group[old_group]  -= self.successors_of[word][word]
+        self.predecessor_word_count_of_group[new_group] += self.successors_of[word][word]
+        self.succsessor_word_count_of_group[new_group]  += self.successors_of[word][word]
+
 
     def calc_perplexity(self):
         sum1 = sum2 = 0
@@ -220,6 +218,7 @@ class Learner():
         did_update = True
         while did_update:
             did_update = False
+            print self.group_bigramm_counts
 
             for word in self.vocabulary:
                 min_perplexity       = perpelxity
@@ -227,26 +226,80 @@ class Learner():
                 min_perplexity_group = old_group
                 
                 self.update_help_counts(word)
-                self.remove_word_from_group(word, old_group)
 
                 for group in self.group_counts:                    
                     if group != old_group:
-                        self.add_word_to_group(word, group)
+                        self.move_word(word, group)
                         temp_perpelxity = self.calc_perplexity()
-                        self.remove_word_from_group(word, group)
 
                         if temp_perpelxity < min_perplexity:
                             min_perplexity       = temp_perpelxity
                             min_perplexity_group = group   
 
-                # move word from its group to group which results in minimum perlexity
+                # move word from its group to group which results in minimum perpelxity
                 perpelxity = min_perplexity
                 
-                self.add_word_to_group(word, min_perplexity_group)
-                self.vocabulary[word].group = min_perplexity_group
+                self.move_word(word, min_perplexity_group)
                 if min_perplexity_group != old_group:
                     print "move " + word + " from group " + str(old_group) + " to group " + str(min_perplexity_group)
                     did_update = True
-            print "---"
+            print ("---", perpelxity)
+            print self.group_bigramm_counts
+            break
+
+    def print_gropus(self):
+        groups = defaultdict(list)
+
+        for word, wordData in self.vocabulary.iteritems():
+            groups[wordData.group].append(word)
+
+        for group in groups:
+            print (group, groups[group])
+
+    def stupid_test(self):
+        self.setup_inital_data()
+
+        print self.group_bigramm_counts
+        print ("3:", self.group_counts[3], "2")
+        print ("0:", self.group_counts[0], "13")
+        print ("0_0:", self.group_bigramm_counts["0_0"], "5")
+        print ("0_3:", self.group_bigramm_counts["0_3"], "2")
+        print ("3_17:", self.group_bigramm_counts["3_17"], "1")
+        print ("0_17:", self.group_bigramm_counts["0_17"], "0")
+        print ("0_1:", self.group_bigramm_counts["0_1"], "1")
+
+        print "---- Altweibersommer from 3 to 0"
+        self.update_help_counts("Altweibersommer")
+        self.move_word("Altweibersommer", 0)
+        print ("3:", self.group_counts[3], "0")
+        print ("0:", self.group_counts[0], "15")
+        print ("0_0:", self.group_bigramm_counts["0_0"], "7")
+        print ("3_17:", self.group_bigramm_counts["3_17"], "0")
+        print ("0_17:", self.group_bigramm_counts["0_17"], "1")
+        print ("0_1:", self.group_bigramm_counts["0_1"], "2")
+        print ("0_2:", self.group_bigramm_counts["0_2"], "1")
+        
+
+        print "---- Altweibersommer from 0 to 2"
+        self.move_word("Altweibersommer", 2)
+        print self.succsessor_word_count_of_group[0]
+        print ("0:", self.group_counts[0], "13")
+        print ("2:", self.group_counts[2], "4")
+        print ("0_2:", self.group_bigramm_counts["0_2"], "3")
+        print ("3_17:", self.group_bigramm_counts["3_17"], "0")
+        print ("2_17:", self.group_bigramm_counts["2_17"], "1")
+        print ("2_1:", self.group_bigramm_counts["2_1"], "1")
+
+        print "---- Altweibersommer from 2 to 1"
+        self.move_word("Altweibersommer", 1)
+        print "----"
+        print self.group_bigramm_counts
+        print ("2:", self.group_counts[2], "2")
+        print ("1:", self.group_counts[1], "6")
+        print ("0_3:", self.group_bigramm_counts["0_3"], "0")
+        print ("3_17:", self.group_bigramm_counts["3_17"], "0")
+        print ("0_1:", self.group_bigramm_counts["0_1"], "3")
+        print ("1_17:", self.group_bigramm_counts["1_17"], "1")
+
 
 Learner()
